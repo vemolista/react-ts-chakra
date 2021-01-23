7596fb2132d039cc5a60ad8c7df47dadc73806b7
Tomas Vemola <vemola1@seznam.cz>
2021-01-23 11:10:55 +0100

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+	<ChakraProvider>
+		<App />
+	</ChakraProvider>,
+	document.getElementById('root')
+);
